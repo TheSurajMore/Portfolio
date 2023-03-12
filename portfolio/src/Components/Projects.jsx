e88286b765edc1feba6a410f200969a9f0180dcf
TheSurajMore <surajmbusiness@gmail.com>
2023-03-12 15:04:05 +0530

    Update

--- portfolio/src/Components/Projects.jsx
+++ portfolio/src/Components/Projects.jsx
@@ -10,22 +10,22 @@ const Projects = () => {
       <Heading>E-COMMERCE</Heading>
       <Box>⦿ React Web App</Box>
       <Box>⦿ Solo Project</Box>
-      <Box>⦿ Technologies used:- React, Redux, JavaScript, CSS, HTML, Responsive UI, react-router-dom, Libraries. </Box>
-      <Button mb={'1%'} mt={'1%'} colorScheme='blue'>Open Live Project In New Tab<ExternalLinkIcon/> </Button>
-    </Box>
-    <Box borderWidth={'5px'} mt={'1%'} >
-      <Heading>YOUTUBE CLONE</Heading>
-      <Box>⦿ React Web App</Box>
-      <Box>⦿ Solo Project</Box>
-      <Box>⦿ Technologies used:- React, Redux, JavaScript, CSS, HTML, Responsive UI, react-router-dom, Libraries.  </Box>
-      <Button mb={'1%'} mt={'1%'} colorScheme='blue'>Open Live Project In New Tab<ExternalLinkIcon/> </Button>
+      <Box>⦿ Technologies used:- React, Context, JavaScript, CSS, HTML, Responsive UI, react-router-dom, Libraries. </Box>
+      <a href="https://surajecommerce.netlify.app/" target={'_blank'} rel='noreferrer' ><Button mb={'1%'} mt={'1%'} colorScheme='blue'>Open Live Project In New Tab <ExternalLinkIcon/> </Button></a>
     </Box>
     <Box borderWidth={'5px'} mt={'1%'} >
       <Heading>MY TODO</Heading>
       <Box>⦿ React Web App</Box>
       <Box>⦿ Solo Project</Box>
-      <Box>⦿ Technologies used:- React, Redux, JavaScript, CSS, HTML, Responsive UI, react-router-dom, Libraries.  </Box>
-      <Button mb={'1%'} mt={'1%'} colorScheme='blue'>Open Live Project In New Tab<ExternalLinkIcon/> </Button>
+      <Box>⦿ Technologies used:- React, Redux, JavaScript, CSS, HTML, Responsive UI, Libraries.  </Box>
+      <a href="https://surajtodo.netlify.app/" target={'_blank'} rel='noreferrer' ><Button mb={'1%'} mt={'1%'} colorScheme='blue'>Open Live Project In New Tab <ExternalLinkIcon/> </Button></a>
+    </Box>
+    <Box borderWidth={'5px'} mt={'1%'} >
+      <Heading>YOUTUBE CLONE</Heading>
+      <Box>⦿ React Web App</Box>
+      <Box>⦿ Solo Project</Box>
+      <Box>⦿ Technologies used:- React, JavaScript, CSS, HTML, Responsive UI, Libraries.  </Box>
+      <a href="https://clone-e822e.web.app/" target={'_blank'} rel='noreferrer' ><Button mb={'1%'} mt={'1%'} colorScheme='blue'>Open Live Project In New Tab <ExternalLinkIcon/> </Button></a>
     </Box>
   </Box>
   </Center>
